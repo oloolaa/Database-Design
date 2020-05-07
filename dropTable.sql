fc38cf9b74c7d5ae81f619a1f1b8602c1c3bccce
Yinglue (Jeremy) <565711805@qq.com>
2020-05-06 21:12:31 -0500

    Add files via upload

---
+++ dropTable.sql
@@ -0,0 +1,45 @@
+DROP TABLE CartBook CASCADE constraints PURGE;
+
+DROP TABLE ShoppingCart CASCADE constraints PURGE;
+
+DROP TABLE AccountType CASCADE constraints PURGE;
+
+DROP TABLE Account CASCADE constraints PURGE;
+
+DROP TABLE AccountAddress CASCADE constraints PURGE;
+
+DROP TABLE AccountPhone CASCADE constraints PURGE;
+
+DROP TABLE Discount CASCADE constraints PURGE;
+
+DROP TABLE Review CASCADE constraints PURGE;
+
+DROP TABLE Book CASCADE constraints PURGE;
+
+DROP TABLE BookGenre CASCADE constraints PURGE;
+
+DROP TABLE Publisher CASCADE constraints PURGE;
+
+DROP TABLE PublisherAddress CASCADE constraints PURGE;
+
+DROP TABLE Department CASCADE constraints PURGE;
+
+DROP TABLE Genre CASCADE constraints PURGE;
+
+DROP TABLE OrderHistory CASCADE constraints PURGE;
+
+DROP TABLE OrderBook CASCADE constraints PURGE;
+
+DROP TABLE Shipment CASCADE constraints PURGE;
+
+DROP TABLE Payment CASCADE constraints PURGE;
+
+DROP TABLE BankAccount CASCADE constraints PURGE;
+
+DROP TABLE Giftcard CASCADE constraints PURGE;
+
+DROP TABLE EPay CASCADE constraints PURGE;
+
+DROP TABLE Address CASCADE constraints PURGE;
+
+DROP TABLE AccountPaymentType CASCADE constraints PURGE;
